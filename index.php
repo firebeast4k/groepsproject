4b92e604970461a17db16c6ddff9855b6243fbce
firebeast4k <64073702+firebeast4k@users.noreply.github.com>
2021-11-12 13:57:03 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>Document</title>
+    <link rel="stylesheet" href="content/CSS/gip.css">
+
+</head>
+
+<body>
+
+<div id="overkoepelende">
+    <?php
+        include ('content/header.html');
+        include ('content/container.html');
+        include ('content/footer.html');
+    ?>
+</div>
+    
+</body>
+
+</html>
